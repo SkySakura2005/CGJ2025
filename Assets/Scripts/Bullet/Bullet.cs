b054702df82c3010b2715ea28f4c9c6af48454d6
SkySakura <1693385825@qq.com>
2025-06-28 01:39:04 +0800

    Remaining files
    Signed-off-by: SkySakura <1693385825@qq.com>

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,7 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
 namespace Bullet
 {
-    public class Bullet
+    public class Bullet:MonoBehaviour
     {
+        public float InitSpeed=10;
         
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.velocity=new Vector2(5*Mathf.Cos(Mathf.PI/4f),5*Mathf.Sin(Mathf.PI/4f));
+        }
+
+        private void Update()
+        {
+            if (_rb.velocity.magnitude <= 0.1f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
