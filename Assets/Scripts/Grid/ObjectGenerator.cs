2a98e30ce01bd2c9d0cc493875e7932565254c73
Hylin0918 <Hylin_0918@163.com>
2025-06-29 04:32:39 +0800

    部分ui功能

--- Assets/Scripts/Grid/ObjectGenerator.cs
+++ Assets/Scripts/Grid/ObjectGenerator.cs
@@ -20,9 +20,24 @@ namespace Grid
             GenerateRandomObject();
         }
 
-        private void GenerateRandomObject()
+        public void GenerateRandomObject()
         {
-            
+            Array values = Enum.GetValues(typeof(BuffType));
+            BuffType randomBuffType;
+            do
+            {
+                randomBuffType = (BuffType)values.GetValue(Random.Range(0, values.Length));
+            } while (randomBuffType == BuffType.None);
+
+            string prefabPath = "";//路径，需要修改
+
+            GameObject prefab = Resources.Load<GameObject>(prefabPath);
+
+            GameObject newObject = Instantiate(prefab, transform, true);
+            newObject.transform.position = new Vector3(
+                Random.Range(_coll.bounds.min.x, _coll.bounds.max.x),
+                Random.Range(_coll.bounds.min.y, _coll.bounds.max.y)
+            );
         }
         public void GenerateObject(IObjectType objectType)
         {
